a00a3a21c03224f142d75e93a64be3942fa41df0
Shaheel <chicooryyashod@gmail.com>
2023-12-16 22:54:33 -0500

    corrected navbar

--- views/Login.php
+++ views/Login.php
@@ -25,18 +25,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="cache-control" content="no-cache">
-    <title>KsWears</title>
+    <title>SportShoeHub</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../public/css/style.css">
     <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <a class="navbar-brand" href="../index.php"><b><span class="firstWord">KS WEARS</span></b><span class="secondWord">
-                    WORKOUT IN STYLE</span></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <a class="navbar-brand" href="../index.php"><b><span class="firstWord">SportShoeHub</span></b><span class="secondWord">
+                STEP INTO PERFORMANCE</span></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
--- views/includes/Header.php
+++ views/includes/Header.php
@@ -1,4 +1,3 @@
-<!--Header and Navigation Section-->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,17 +5,17 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="cache-control" content="no-cache">
-    <title>KsWears</title>
+    <title>SportShoeHub</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../public/css/style.css">
 </head>
+
 <header>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <a class="navbar-brand" href="../index.php"><b><span class="firstWord">KS WEARS</span></b><span class="secondWord">
-                WORKOUT IN STYLE</span></a>
+        <a class="navbar-brand" href="../index.php"><b><span class="firstWord">SportShoeHub</span></b><span class="secondWord">
+                STEP INTO PERFORMANCE</span></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -38,18 +37,17 @@
                     </a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-database-fill-lock"></i> Administration</a>
+                    <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-gear"></i> Administration</a>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="ManageProducts.php"><i class="bi bi-basket-fill"></i> Manage Products</a>
                         </li>
-                        <li><a class="dropdown-item" href="ManageUsers.php"><i class="bi bi-people"></i> Users</a></li>
+                        <li><a class="dropdown-item" href="ManageUsers.php"><i class="bi bi-person"></i> Users</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item" href="manageCommands.php"><i class="bi bi-bag-fill"></i> Orders</a></li>
+                        <li><a class="dropdown-item" href="manageCommands.php"><i class="bi bi-box"></i> Orders</a></li>
                     </ul>
                 </li>
-                <li class="nav-item">
                 <li class="nav-item">
                     <a class="nav-link active" href="Logout.php"><i class="bi bi-box-arrow-right"></i> Log Out</a>
                 </li>
